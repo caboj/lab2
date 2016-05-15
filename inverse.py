47c27cc9b51cea448ba3f129adb608054463dec8
caboj <jacob.verdegaal@student.uva.nl>
2016-05-15 13:07:02 +0200

    working encoder

--- inverse.py
+++ inverse.py
@@ -31,8 +31,9 @@ def load_data():
             trainD.extend(np.array([re.sub("[^a-zA-Z]", " " , l).lower().split() for l in  f.readlines()]))
 
     trainD = np.array(trainD)
-    voc = np.unique(np.hstack(trainD))
-    
+    voc = np.unique(np.hstack(trainD)).tolist()
+    voc.append('<BOS>')
+    #print(voc)
     y = np.eye(len(voc))
     vecs = {voc[i] : y[i] for i in range(len(voc))}
 
@@ -90,22 +91,61 @@ class Encoder(object):
     def get_parameters(self):
         return [self.W, self.U]
 
+class Decoder(object):
+    def __init__(self, w_init, u_init):
+        self.O = theano.shared(w_init())
+        self.V = theano.shared(u_init())
+        self.Yh = theano.shared(w_init())
+        
+    def get_output_expr(self,h,l):
+        #y0 = theano.shared(vecs['<BOS>'])
+        y0 = T.zeros((self.O.shape[1], ))
+
+        h0 = h
+        
+        self.c = h
+        [y,h], _ = theano.scan(fn=self.__get_rnn_step_expr,
+                           #sequences=input_sequence,
+                           #non_sequences=[self.O,self.V,self.Yh],
+                           n_steps = l,
+                           outputs_info=[y0,h0])
+        return y
+
+    def __get_rnn_step_expr(self, y_tm1, h_tm1):
+        y_t = T.tanh(T.dot(self.O,h_tm1)+T.dot(self.Yh,y_tm1))
+        h_t = T.tanh(T.dot( self.V,h_tm1))
+        return [y_t,h_t]
+        
+    def get_parameters(self):
+        return [self.W, self.U]
+
 
 def train():
     #embedding_layer = EmbeddingLayer(embedding_init)
     encoder = Encoder(w_init, u_init)
+    decoder = Decoder(w_init, u_init)
     x = T.imatrix()
-    
+
     #embedding_expr = embedding_layer.get_output_expr(x)
     h=encoder.get_output_expr(x)
     encode = theano.function(inputs=[x],outputs=[h])    
+
+    #l = 1
+    #y=decoder.get_output_expr(h,l)
+    #decode = theano.function(inputs=[h,l],outputs=[y])    
     
     for sen in trainD:
         x = np.array([vecs[sen[i]] for i in range(len(sen))],dtype=np.int32)
-        encode(x)
+        H = encode(x)
+        #print('H: ', H[0][-1])
+        #print(h)
+        #   l = len(sen)
+        #decode(H[0][-1],l)
 
-    print(h[-1])
+    print(H)
+    
 
+    
 if __name__ == "__main__":
     args = parser.parse_args()
     nr_hidden = args.nr_hidden
